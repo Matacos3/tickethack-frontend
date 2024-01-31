633534dbd080bfab09c9fb5bcd4632a0af544406
Matacos3 <acosta.garcia.mateo@gmail.com>
2024-01-31 12:22:09 +0100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+function setEvent(){
+  
+  for (let i = 0; i < document.querySelectorAll('.btn-book').length; i++) {
+    document.querySelectorAll('.btn-book')[i].addEventListener('click',
+      function () {
+        fetch(`http://localhost:3000/cart/${this.parentNode.id}`)
+        .then(response => response.json())
+        .then(data =>{
+          console.log("success")
+          window.location.assign("./cart.html")
+        })
+      }
+    );
+   }
+}
+
 document.querySelector("#submit").addEventListener("click", e=>{
+  //je crée une variable contenant les informations sur le voyage que l’on recherche
   const lookedForTrip={
-    Departure : document.querySelector("#departure-city").value,
-    Arrival : document.querySelector("#arrival-city").value,
-    Date : new Date(document.querySelector("#date-of-trip").value)
+    departure : document.querySelector("#departure-city").value,
+    arrival : document.querySelector("#arrival-city").value,
+    date : new Date(document.querySelector("#date-of-trip").value)
   }
 
-  console.log(lookedForTrip);
+  //maintenant, je vais solliciter la route qui nous intéresse
 
+  fetch("http://localhost:3000/trips",{
+    method : "POST",
+    headers: {"Content-Type" : "application/json"},
+    body: JSON.stringify(lookedForTrip)})
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      // document.querySelector("#default").style.display = "none";
+      if(data.result === false){
+        document.querySelector("#no-train").style.display = "flex"
+        document.querySelector("#result-display-found").style.display = "none";
+
+      } else{
+        document.querySelector("#default").style.display = "none";
+        document.querySelector("#no-train").style.display = "none";
+        document.querySelector("#result-display-found").style.display = "flex";
+        document.querySelector("#result-display-found").innerHTML = "";
+        const trips = data.allTrips;
+        for(let i = 0; i< trips.length; i++){
+          console.log(trips[i])
+          const minutes = new Date(trips[i].date).getMinutes();
+          document.querySelector("#result-display-found").innerHTML +=`
+            <div class="available-trip" id="${trips[i]._id}">
+              <p>${trips[i].departure} > ${trips[i].arrival}</p>
+              <p> ${new Date(trips[i].date).getHours()}:${ minutes > 9 ? minutes : "0" + minutes} </p>
+              <p>${trips[i].price}€</p>
+              <button class="btn btn-book">Book</button>
+            </div>
+          `
+        }
+          setEvent();
+      }
+      
+    })
   
 
-console.log(typeof lookedForTrip.Date)
 })
+
